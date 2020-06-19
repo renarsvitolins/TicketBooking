d096d2dfc31b9b2c92db8be4caf596cdb2ffcec1
renar <renar@DESKTOP-1S8R694>
2020-06-19 15:22:06 +0300

    The start

---
+++ TicketBookingSystem/src/main/java/lv/venta/demo/TicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package lv.venta.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingSystemApplication.class, args);
+	}
+
+}
